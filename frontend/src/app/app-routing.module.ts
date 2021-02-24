c4345346135657f003584fb920193b9cf0b4903c
Your Name <you@example.com>
2021-02-24 08:01:55 +0400

    adding frontend components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { SpecialComponent } from './special/special.component';
+import { EventsComponent } from './events/events.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'events', pathMatch: 'full' },
+  { path: 'events', component: EventsComponent },
+  { path: 'special', component: SpecialComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
